1dd4581397da71a650da999226964037a415b2b2
WendyKariuki <phaminuswendy@gmail.com>
2024-03-25 14:05:55 +0300

    Filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+ 
+ function findMatching() {
+    const drivers = [ "Kim", "Tom", "Kyle", "Bob", "Sam", "Sally"
+    ]
+
+ }
+
+ function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+
+    function matchName(drivers, query) {
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
